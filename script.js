38c03b12f78a10267810e961a0b415a5459bf995
Ashok Kushwaha <2201ee18_ashok@iitp.ac.in>
2025-03-28 16:14:33 +0530

    edit button succesfully added

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
 const addTaskBTn=document.getElementById('add-task-button');
 
 const todoBoard=document.getElementById('todo-board');
+
+ function attactTagretEvent(target){
+    target.addEventListener('dragstart',() => {
+        target.classList.add('flying');
+    });
+      target.addEventListener('dragend',() => {
+        target.classList.remove('flying');
+    });
+ }
+
 addTaskBTn.addEventListener('click', ()=>{
     const input = prompt('what is your task');
     if(!input) return;
 
+   const itemCard=document.createElement('div');
+   itemCard.classList.add('item-card');
 
     const taskCard=document.createElement('p');
     taskCard.classList.add('item');
     taskCard.innerText=input;
-    taskCard.setAttribute('draggable', true);
-    todoBoard.appendChild(taskCard);
+   
+    const editButton=document.createElement('button');
+    editButton.classList.add('edit-task-button');
+    editButton.innerText="edit";
+    
+     
+    itemCard.setAttribute('draggable', true);
+   attactTagretEvent(itemCard);
+     
+   
+    itemCard.appendChild(taskCard);
+    itemCard.appendChild(editButton);
+    todoBoard.appendChild(itemCard);
+
 
 });
 
 
 const allBoard=document.querySelectorAll('.board');
-const allItems=document.querySelectorAll('.item');
-allItems.forEach((item) => {
-    item.addEventListener('dragstart',() => {
-        item.classList.add('flying');
-    });
-    item.addEventListener('dragend',() => {
-        item.classList.remove('flying');
-    });
+const allItemscards=document.querySelectorAll('.item-card');
 
-})
+allItemscards.forEach(attactTagretEvent);
 allBoard.forEach((board) =>{
     
-    board.addEventListener('dragover',(event) => {
-        event.preventDefault(); 
-         const flyingElement=document.querySelector('.flying');
-         console.log(board,'kuch to gya');
-         board.appendChild(flyingElement);
+    board.addEventListener('dragover',() => {
+       // event.preventDefault(); 
+        const flyingElement=document.querySelector('.flying');
+        console.log(board,'kuch to gya');
+
+        board.appendChild(flyingElement);
+          
     });
 });
